ffc2c987ef3392335bc88d8c9a1b86a3a869cedc
Gargenziano03 <gargenziano03@gmail.com>
2024-10-04 17:17:46 +0200

    add function

--- palidroma/main.js
+++ palidroma/main.js
@@ -1,1 +1,27 @@
-console.log('ciao');
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+
+/*PRE-FASE: STRUMENTI 
+--let / const
+-- .prompt
+-- function
+-- if / else 
+*/
+
+//FASE 1: PREPARAZIONE 
+let parolaUtente
+let parolaMinuscola
+
+//FASE 2: RACCOLTA DATI 
+
+//FASE 3: ELABORAZIONE DATI
+ function verifica(word) {
+    parolaMinuscola = word.toLowerCase();
+    return parolaUtente === parolaMinuscola.split('').reverse().join('');
+ }
+
+
+//FASE 4: OUTPUT
