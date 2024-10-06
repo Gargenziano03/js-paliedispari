cd5d7132436e49ac05b37194da9973c7b25433be
Gargenziano03 <gargenziano03@gmail.com>
2024-10-06 17:33:42 +0200

    Add scelta and numeroutente

--- pari_dispari/main.js
+++ pari_dispari/main.js
@@ -15,8 +15,43 @@ Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari
 */
 
 //FASE 1: PREPARAZIONE 
-
 let scelta ;
 let numeroUtente;
 let numeroComputer; 
 let sum;
+let messagge;
+
+//FASE 2: RACCOLTA DATI 
+// l'utente sceglie pari o dispari
+scelta = prompt('Scegli pari o dispari');
+alert(scelta);
+console.log(scelta);
+// l'utente  inserisce un numero da 1 a 5
+numeroUtente = Number(prompt('scegli un numero da 1 a 5'));
+
+
+
+//FASE 3: ELABORAZIONE DATI
+function maggioreMinore(numeroUtente) {
+    // verifica se l' elemento non è un numero
+    if(!isNaN(numeroUtente) ){
+        // verifica se il numero inserito è maggiore o 1 ANd
+        // se il numeri inserito è minore o unguale a 5
+        // se riusculta falso 
+        if ( numeroUtente >= 1 && numeroUtente <= 5) {
+            alert(numeroUtente);
+            console.log(numeroUtente)
+        // allora stampa il numero deve essere compreso tra 1 e 5
+        }else{
+            alert('il numero deve essere compreso tra 1 e 5')
+        }
+    //se non è un numero stampa il messaggio
+    }else{
+        messagge = 'elemento inserito non valido';
+        alert(messagge);
+        console.log(messagge);
+    }   
+}
+maggioreMinore(numeroUtente);
+
+
